7f4c1f8c3009be7ee1396937c35d58a43d873b8c
tinect <s.koenig@tinect.de>
2025-12-29 01:26:45 +0100

    feat: simplify EmailChecker result creation feat: do not detect roleAccount, disposable and b2b if no mx records are found

--- src/EmailChecker.php
+++ src/EmailChecker.php
@@ -12,63 +12,39 @@ readonly class EmailChecker
 
     public function check(string $email): Result
     {
+        $result = new Result(input: $email);
+
         $syntax = new Syntax($email);
-        if (!$syntax->isValid()) {
-            return new Result(
-                input: $email,
-                isSyntaxValid: false,
-                hasMxRecords: false,
-                isReachable: false,
-                isCatchAll: false,
-                isDisposable: false,
-                isB2C: false,
-                isRoleAccount: false,
-                isDisabled: false,
-                hasFullInbox: false,
-                error: 'Invalid syntax'
-            );
+        $result->isSyntaxValid = $syntax->isValid();
+
+        if ($result->isSyntaxValid === false) {
+            $result->error = 'Invalid syntax';
+
+            return $result;
         }
 
-        $miscIsRole = $this->misc->isRoleAccount($syntax->username);
-        $miscIsDisposable = $this->misc->isDisposable($email);
-        $miscIsB2C = $this->misc->isB2C($syntax->domain);
-
         $mxRecords = $this->dns->getMxRecords($syntax->domain);
-        if (empty($mxRecords)) {
-            return new Result(
-                input: $email,
-                isSyntaxValid: true,
-                hasMxRecords: false,
-                isReachable: false,
-                isCatchAll: false,
-                isDisposable: $miscIsDisposable,
-                isB2C: $miscIsB2C,
-                isRoleAccount: $miscIsRole,
-                isDisabled: false,
-                hasFullInbox: false,
-                error: 'No MX records found'
-            );
+        $result->hasMxRecords = !empty($mxRecords);
+
+        if ($result->hasMxRecords === false) {
+            $result->error = 'No MX records found';
+
+            return $result;
         }
 
         $mxHost = $mxRecords[0];
-
         $smtpDetails = $this->smtp->check($syntax->domain, $mxHost, $email);
 
-        $isReachable = $smtpDetails['can_connect'] && $smtpDetails['is_deliverable'];
+        $result->mxHost = $mxHost;
+        $result->isRoleAccount = $this->misc->isRoleAccount($syntax->username);
+        $result->isDisposable = $this->misc->isDisposable($email);
+        $result->isB2C = $this->misc->isB2C($syntax->domain);
+        $result->isReachable = $smtpDetails['can_connect'] && $smtpDetails['is_deliverable'];
+        $result->isCatchAll = $smtpDetails['is_catch_all'];
+        $result->isDisabled = $smtpDetails['is_disabled'];
+        $result->hasFullInbox = $smtpDetails['has_full_inbox'];
+        $result->error = $smtpDetails['error'];
 
-        return new Result(
-            input: $email,
-            isSyntaxValid: true,
-            hasMxRecords: true,
-            isReachable: $isReachable,
-            isCatchAll: $smtpDetails['is_catch_all'],
-            isDisposable: $miscIsDisposable,
-            isB2C: $miscIsB2C,
-            isRoleAccount: $miscIsRole,
-            isDisabled: $smtpDetails['is_disabled'],
-            hasFullInbox: $smtpDetails['has_full_inbox'],
-            mxHost: $mxHost,
-            error: $smtpDetails['error']
-        );
+        return $result;
     }
 }
